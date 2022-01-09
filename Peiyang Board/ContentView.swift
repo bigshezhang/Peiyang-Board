2390f602278daec79d70630042fa722923e100a7
bigshezhang <bigshezhang@gmail.com>
2022-01-10 00:29:02 +0800

    Initial Commit

---
+++ Peiyang Board/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Peiyang Board
+//
+//  Created by 李子鸣 on 2022/1/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peiyang Board/Peiyang_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peiyang_BoardApp.swift
+//  Peiyang Board
+//
+//  Created by 李子鸣 on 2022/1/10.
+//
+
+import SwiftUI
+
+@main
+struct Peiyang_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
