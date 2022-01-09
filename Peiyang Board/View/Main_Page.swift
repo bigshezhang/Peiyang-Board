5feb6f566b2c45a15f2741e2fd5c3de07c3daa11
bigshezhang <bigshezhang@gmail.com>
2022-01-10 02:44:50 +0800

    First Step

--- Peiyang Board/View/Main_Page.swift
+++ Peiyang Board/View/Main_Page.swift
@@ -9,9 +9,62 @@ import SwiftUI
 
 struct Main_Page: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false){
+            VStack{
+                Head_view();
+            }
+        }
+        
+        
     }
 }
+
+
+@ViewBuilder
+func Head_view()->some View{
+    HStack(){
+        VStack{
+            Text("\(time.month)月\(time.day)日")
+                .font(.system(size: 40))
+                .foregroundColor(Color("Main_Font"))
+                .position(x: 100, y: 40)
+            Path{path in
+                path.move(to: CGPoint(x: 35, y: 30))
+                path.addLine(to: CGPoint(x: 163, y: 30))
+                
+            }
+            .strokedPath(StrokeStyle(lineWidth: 2.5, lineCap: .round, lineJoin: .round))
+            .fill(Color("Main_Font"))
+            
+            HStack{
+                Text("总消息数")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color("Main_Font"))
+                    .position(x: 70, y: 5)
+                
+                Text("\(Information.count)")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color("Main_Font"))
+                    .position(x: 60, y: 5)
+            }
+        }
+        HStack{
+            ZStack{
+                Text("@")
+                    .font(.system(size: 100))
+                    .foregroundColor(Color("Main_Font"))
+                    .position(x: 120, y: 44)
+                
+                Image("user")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .position(x: 120, y: 50)
+            }
+        }
+    }
+}
+
 
 struct Main_Page_Previews: PreviewProvider {
     static var previews: some View {
